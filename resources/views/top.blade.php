421a5dff2dbbe1351ac294266316b3b84a7b8fde
tomo-john <keririnkery.kery@gmail.com>
2026-05-25 07:14:49 +0900

    style: 仮のTop画面

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,6 +1,25 @@
 <x-layouts.base>
     <div x-data="start()">
-        <i class="fa-solid fa-dog"></i>
+
+        <div class="flex items-center justify-center gap-1 m-10">
+            <h1 class="text-xl font-bold text-slate-500">
+                <i class="fa-solid fa-dog"></i>
+                Trace Note
+            </h1>
+        </div>
+
+        <div class="flex items-center justify-center gap-4">
+            <a href="{{ route('login') }}"
+               class="inline-block w-28 text-center bg-amber-400 text-white rounded-lg py-2 px-5 hover:bg-amber-500 transition"
+            >
+                ログイン
+            </a>
+            <a href="{{ route('register') }}"
+               class="inline-block w-28 text-center bg-slate-400 text-white rounded-lg py-2 px-5 hover:bg-slate-500 transition"
+            >
+                新規登録
+            </a>
+        </div>
     </div>
 
     <script>
