cff0533d5cf897e8be48dbfa23429c0ee10cc208
tomo-john <keririnkery.kery@gmail.com>
2026-05-26 13:20:57 +0900

    chore: Traceモデル・ルーティング修正
    - status用メソッド追加
    - index, createのルーティング追加

--- app/Models/Trace.php
+++ app/Models/Trace.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Trace extends Model
 {
@@ -13,9 +14,26 @@ class Trace extends Model
         'status',
     ];
 
-    protected $casts = [
-        'status' => 'string',
-    ];
+    // status
+    const STATUS_DRAFT = 'draft';
+    const STATUS_RESEARCHING = 'researching';
+    const STATUS_UNDERSTOOD = 'understood';
+    const STATUS_PRACTICED = 'practiced';
+
+    public static function statuses(): array
+    {
+        return[
+            self::STATUS_DRAFT => '下書き',
+            self::STATUS_RESEARCHING => '調査中',
+            self::STATUS_UNDERSTOOD => '理解済み',
+            self::STATUS_PRACTICED => '実践済み',
+        ];
+    }
+
+    public function statusLabel(): string
+    {
+        return self::statuses()[$this->status] ?? '不明';
+    }
 
     public function user() :BelongsTo
     {
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Trace\Index;
+use App\Livewire\Trace\Create;
 
 Route::view('/', 'top')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::get('traces/index', Index::class)->name('trace.index');
+    Route::get('traces/create', Create::class)->name('trace.create');
 });
 
 require __DIR__.'/settings.php';
