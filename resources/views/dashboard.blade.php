dd5433a223dfdb5ed4aa5b32754848c38a1ec048
tomo-john <keririnkery.kery@gmail.com>
2026-06-02 08:07:56 +0900

    chore: ダッシュボードにTrace, Tagの数を表示

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,11 +2,12 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 flex items-center justify-center gap-2">
-                <i class="fa-solid fa-dog text-pink-400"></i>
-                <span>{{ App\Models\Trace::count() }}</span>
+                <i class="fa-solid fa-dog text-sky-400"></i>
+                <span>{{ auth()->user()->traces()->count() }} Traces</span>
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 flex items-center justify-center gap-2">
+                <i class="fa-solid fa-dog text-sky-400"></i>
+                <span>{{ auth()->user()->tags()->count() }} Tags</span>
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
