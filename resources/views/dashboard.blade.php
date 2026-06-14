40922bb5b77cdaa1605780e01a8cbeb9fde31e9c
tomo-john <keririnkery.kery@gmail.com>
2026-06-15 06:45:51 +0900

    sytle: レイアウト変更

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,36 +1,73 @@
 <x-layouts::app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
 
-        {{-- 現在のステータス --}}
-        <div class="relative h-full rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <div class="rounded-xl p-6">
+        <div class="grid gap-4 lg:grid-cols-2">
 
-                <h2 class="font-bold text-lg mb-4">
-                    Dashboard
-                </h2>
+            {{-- Dashboard --}}
+            <div class="rounded-xl border border-neutral-200 dark:border-neutral-700">
+                <div class="rounded-xl p-6">
 
-                <div class="flex flex-col gap-4">
-                    <div class="flex flex-wrap items-center gap-2">
-                        <a href="{{ route('trace.index') }}">
-                            <i class="fa-solid fa-dog text-sky-400 w-6"></i>
-                            <span>{{ $traceCount }} Traces</span>
-                        </a>
-                        @foreach($statusCounts as $status)
-                            <div class="w-12 text-center text-sm p-1 rounded-full {{ $status['colorClass'] }}">
-                                <i class="{{ $status['iconClass'] }}"></i>
-                                <span class="">{{ $status['count'] }}</span>
-                            </div>
-                        @endforeach
-                    </div>
+                    <h2 class="font-bold text-lg mb-4">
+                        Dashboard
+                        <i class="fa-solid fa-dog"></i>
+                    </h2>
 
-                    <div>
-                        <a href="{{ route('tag.index') }}">
-                            <i class="fa-solid fa-tag text-sky-400 w-6"></i>
-                            <span>{{ $tagCount }} Tags</span>
-                        </a>
+                    {{-- Trace Tag Count --}}
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-wrap items-center gap-2">
+                            <a href="{{ route('trace.index') }}">
+                                <i class="fa-solid fa-dog text-sky-400 w-6"></i>
+                                <span>{{ $traceCount }} Traces</span>
+                            </a>
+                            @foreach($statusCounts as $status)
+                                <div class="w-12 text-center text-sm p-1 rounded-full {{ $status['colorClass'] }}">
+                                    <i class="{{ $status['iconClass'] }}"></i>
+                                    <span class="">{{ $status['count'] }}</span>
+                                </div>
+                            @endforeach
+                        </div>
+
+                        <div>
+                            <a href="{{ route('tag.index') }}">
+                                <i class="fa-solid fa-tag text-sky-400 w-6"></i>
+                                <span>{{ $tagCount }} Tags</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
+            </div>
 
+            {{-- Activity History --}}
+            <div class="relative h-full rounded-xl border border-neutral-200 dark:border-neutral-700">
+                <div class="rounded-xl p-6">
+
+                    <h2 class="font-bold text-lg mb-4">
+                        Activity History
+                        <i class="fa-solid fa-dog"></i>
+                    </h2>
+
+                    <div class="flex gap-2">
+                        {{-- 曜日 --}}
+                        <div class="grid grid-rows-7 gap-0 5 text-xs text-slate-500">
+                            <div>Sun</div>
+                            <div>Mon</div>
+                            <div>Tue</div>
+                            <div>Wed</div>
+                            <div>Thu</div>
+                            <div>Fri</div>
+                            <div>Sat</div>
+                        </div>
+
+                        {{-- 草 --}}
+                        <div class="inline-grid grid-flow-col grid-rows-7 gap-0.5">
+                            @foreach($activityCounts as $activity)
+                                <div class="size-3 rounded-sm {{ $activity['colorClass'] }}"
+                                     title="{{ $activity['date'] }} : {{ $activity['count'] }}"
+                                ></div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -83,37 +120,5 @@
             </div>
         </div>
 
-        {{-- 活動履歴 --}}
-        <div class="relative h-full rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <div class="rounded-xl p-6">
-
-                <h2 class="font-bold text-lg mb-4">
-                    活動履歴
-                    <i class="fa-solid fa-dog"></i>
-                </h2>
-
-                <div class="flex gap-2">
-                    {{-- 曜日 --}}
-                    <div class="grid grid-rows-7 gap-0 5 text-xs text-slate-500">
-                        <div>Sun</div>
-                        <div>Mon</div>
-                        <div>Tue</div>
-                        <div>Wed</div>
-                        <div>Thu</div>
-                        <div>Fri</div>
-                        <div>Sat</div>
-                    </div>
-
-                    {{-- 草 --}}
-                    <div class="inline-grid grid-flow-col grid-rows-7 gap-0.5">
-                        @foreach($activityCounts as $activity)
-                            <div class="size-3 rounded-sm {{ $activity['colorClass'] }}"
-                                 title="{{ $activity['date'] }} : {{ $activity['count'] }}"
-                            ></div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </x-layouts::app>
