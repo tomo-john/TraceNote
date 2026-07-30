e140aee8679ec4f954d101e5230026828b11fb2e
tomo-john <keririnkery.kery@gmail.com>
2026-07-30 09:22:16 +0900

    chore: Livewire UI テストページ

--- app/Livewire/Sandbox/Test.php
+++ app/Livewire/Sandbox/Test.php
@@ -6,8 +6,21 @@ use Livewire\Component;
 
 class Test extends Component
 {
- //   public function render()
- //    {
- //        return view('livewire.sandbox.test');
- //    }
+    public bool $showModal = false;
+
+    public function openModal(): void
+    {
+        $this->showModal = true;
+    }
+
+    public function closeModal(): void
+    {
+        $this->showModal = false;
+    }
+
+    public function render()
+     {
+         return view('livewire.sandbox.test')
+            ->layout('components.layouts.base');
+     }
 }
--- resources/views/livewire/sandbox/test.blade.php
+++ resources/views/livewire/sandbox/test.blade.php
@@ -1,9 +1,33 @@
-<div class="max-w-lg w-full mx-auto">
+<div class="py-6 space-y-6">
 
-    <div class="flex items-center justify-center gap-4">
-        <i class="fa-solid fa-dog"></i>
-        <p>レイアウトテスト</p>
-        <i class="fa-solid fa-dog"></i>
-    </div>
+    <x-ui.card class="max-w-xl mx-auto">
+        <div class="flex items-center justify-center gap-2">
+            <i class="fa-solid fa-dog"></i>
+            <p>UI Test Livewire</p>
+            <i class="fa-solid fa-dog"></i>
+        </div>
+    </x-ui.card>
+
+    <x-ui.card class="max-w-xl mx-auto">
+
+        <div class="flex items-center justify-center gap-6">
+
+            <x-ui.button tyep=button wire:click="openModal">
+                Opne Modal
+            </x-ui.button>
+
+            <x-ui.button tyep=button variant="secondary" wire:click="closeModal">
+                Close Modal
+            </x-ui.button>
+
+        </div>
+
+    </x-ui.card>
+
+    @if($showModal)
+        <x-ui.modal maxWidth="sm">
+            <i class="fa-solid fa-dog"></i>
+        </x-ui.modal>
+    @endif
 
 </div>
