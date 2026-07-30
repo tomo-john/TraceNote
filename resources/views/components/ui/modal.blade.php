e95e1bfe4f62309ee6b3625e2ea5497df2cd939a
tomo-john <keririnkery.kery@gmail.com>
2026-07-31 07:09:23 +0900

    style: modalコンポーネント修正

--- resources/views/components/ui/modal.blade.php
+++ resources/views/components/ui/modal.blade.php
@@ -1,5 +1,6 @@
 @props([
     'maxWidth' => 'sm',
+    'close' => '',
 ])
 
 @php
@@ -12,14 +13,14 @@ $width = match($maxWidth) {
     'sm' => 'max-w-sm',
     'md' => 'max-w-xl',
     'lg' => 'max-w-3xl',
-    default => 'max-w-md'
-}
+    default => 'max-w-md',
+};
 
 @endphp
 
 <div class="fixed inset-0 z-50 flex items-center justify-center bg-slate-900/60"
-     @keydown.escape.window="$wire.closeModal()"
-     @click.self="$wire.closeModal()"
+     @keydown.escape.window="$wire.{{ $close }}()"
+     @click.self="$wire.{{ $close }}()"
 >
 
     <x-ui.card {{ $attributes->class(['w-full', $width]) }}>
--- resources/views/livewire/sandbox/test.blade.php
+++ resources/views/livewire/sandbox/test.blade.php
@@ -12,11 +12,11 @@
 
         <div class="flex items-center justify-center gap-6">
 
-            <x-ui.button tyep=button wire:click="openModal">
+            <x-ui.button tyep='button' wire:click="openModal">
                 Opne Modal
             </x-ui.button>
 
-            <x-ui.button tyep=button variant="secondary" wire:click="closeModal">
+            <x-ui.button tyep='button' variant="secondary" wire:click="closeModal">
                 Close Modal
             </x-ui.button>
 
@@ -25,8 +25,20 @@
     </x-ui.card>
 
     @if($showModal)
-        <x-ui.modal maxWidth="sm">
-            <i class="fa-solid fa-dog"></i>
+        <x-ui.modal maxWidth="sm" close="closeModal">
+
+            <div class="flex items-center justify-center gap-2 text-slate-600">
+                <i class="fa-solid fa-dog"></i>
+                Hello Dog
+                <i class="fa-solid fa-dog"></i>
+            </div>
+
+            <div class="flex justify-end">
+                <x-ui.button tyep='button' variant="secondary" wire:click="closeModal">
+                    <i class="fa-solid fa-xmark"></i>
+                </x-ui.button>
+            </div>
+
         </x-ui.modal>
     @endif
 
